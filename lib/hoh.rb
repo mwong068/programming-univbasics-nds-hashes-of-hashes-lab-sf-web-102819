cd71238f562604dfc4a3d245ccc8946adb306791
Megan Wong <mwong068@gmail.com>
2019-10-21 07:38:00 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,17 +47,13 @@ def naming_system
    sub_category: {
      label: "Phylum",
      sub_category: {
-       label: "Class"
+       label: "Class",
        sub_category: {
          label: "Order"
          }
        }
      }
    }
- }
- 
- {
-  label: "Order"
  }
 
 end
